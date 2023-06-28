ad1d0e68034f2023a1621c7494000772366b102b
Pedro Augusto Moura de Oliveira <pedroaugustomoura000@gmail.com>
2023-06-27 22:04:16 -0300

    first commit

---
+++ Desafio1/d1.js
@@ -0,0 +1,33 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (numeroUm == stringUm) {
+    console.log ('1: As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log ('1: As váriaveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+if (numeroTrinta == stringTrinta) {
+    console.log ('30: As várias numeroTrinta e stringTrinta tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log ('30: As várias numeroTrinta e stringTrinta não tem o mesmo valor')
+}
+
+if (numeroDez == stringDez) {
+    console.log('10: As váriaveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log('10: As váriaveis numeroDez e stringDez não tem o mesmo valor')
+}
+
+console.log ('-------------------------------------------------------------------------------')
+//Apenas verificando se todos os tipos estão certos, se não precisa de correção
+console.log(typeof numeroUm)
+console.log(typeof stringUm)
+console.log(typeof numeroTrinta)
+console.log(typeof stringTrinta)
+console.log(typeof numeroDez)
+console.log(typeof stringDez)
